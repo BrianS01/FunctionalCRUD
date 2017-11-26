26510ad4420733cb0f4981a4c772281747a95569
BrianS01 <brian.sterling@correo.usa.edu.co>
2017-11-26 15:14:18 -0500

    Functional

---
+++ script.sql
@@ -0,0 +1,13 @@
+drop database if exists UserDB;
+create database UserDB;
+use UserDB;
+
+CREATE TABLE UserDB.`users` 
+(
+  `userid` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(45) DEFAULT NULL,
+  `lastname` varchar(45) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`userid`)
+);
